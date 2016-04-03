022725ed958fd3a673804c60187300fe59ab74f8
Erik Verheul <erik@verheulconsultants.nl>
2016-04-03 18:08:08 +0200

    Deleted. Now in kunagi2.

--- shared/src/main/java/ilarkesto/core/json/ParseException.java
+++
@@ -1,9 +0,0 @@
-package ilarkesto.core.json;
-
-public class ParseException extends RuntimeException {
-
-	public ParseException(String message, String json, int idx) {
-		super(message + "\n" + idx + ":" + json);
-	}
-
-}
